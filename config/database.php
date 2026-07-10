4db60eb334aec3a5932197e6afca123a97c5a8d6
Yassine Fariss <farissyassine8@gmail.com>
2026-07-10 18:13:46 +0100

    Remove Vercel config, add Railway config with MySQL support

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
+            'url' => env('DB_URL') ?: env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
